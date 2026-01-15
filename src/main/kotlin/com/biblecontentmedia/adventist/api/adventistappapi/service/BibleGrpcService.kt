07673c72d7f4052e1d496d6e2f30f8da783e7a45
Leroy Ramaphoko <adventapp777@gmail.com>
2026-01-15 16:13:43 +0200

    Fix imports

--- src/main/kotlin/com/biblecontentmedia/adventist/api/adventistappapi/service/BibleGrpcService.kt
+++ src/main/kotlin/com/biblecontentmedia/adventist/api/adventistappapi/service/BibleGrpcService.kt
@@ -1,18 +1,14 @@
 package com.biblecontentmedia.adventist.api.adventistappapi.service
 
+import com.biblecontentmedia.adventist.api.grpc.Bible
 import com.biblecontentmedia.adventist.api.grpc.BibleServiceGrpcKt
-import com.biblecontentmedia.adventist.api.grpc.Bible // The outer class
-import com.biblecontentmedia.adventist.api.grpc.Bible.VerseRequest  // Reference the inner message
-import com.biblecontentmedia.adventist.api.grpc.Bible.VerseResponse
 import net.devh.boot.grpc.server.service.GrpcService
 
 @GrpcService
 class BibleGrpcService : BibleServiceGrpcKt.BibleServiceCoroutineImplBase() {
 
-    override suspend fun getVerse(request: VerseRequest): VerseResponse {
-        // For now, returning a hardcoded response as requested.
-        // Later, you will query your database here.
-        return VerseResponse.newBuilder()
+    override suspend fun getVerse(request: Bible.VerseRequest): Bible.VerseResponse {
+        return Bible.VerseResponse.newBuilder()
             .setContent("In the beginning God created the heaven and the earth.")
             .setCitation("Genesis 1:1 (${request.version})")
             .build()
